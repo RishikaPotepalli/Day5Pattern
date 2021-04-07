6708aa42dd199e5e6872634e76b9e3832a4434d4
RishikaPotepalli <82092914+RishikaPotepalli@users.noreply.github.com>
2021-04-07 18:17:50 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,97 @@
+package Q1;
+
+public class Employee {
+	
+	private int empId;
+	private String empName;
+	private String projectName;
+	
+	private int exp;
+	
+	private int salary;
+	
+	
+	public Employee(int id, String name, String pName, int years)
+	{
+		empId = id;
+		empName=name;
+		projectName=pName;
+		exp=years;
+		calculateSalary();
+	}
+	
+	public void calculateSalary()
+	{
+		if(exp<3)
+			setSalary(3000);
+		else if (exp >= 3 && exp <6)
+			setSalary(10000);
+		else if (exp >= 6 && exp <=10)
+			setSalary(15000);
+		else if (exp > 10)
+			setSalary(20000);
+	}
+	
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+
+
+	public int getSalary() {
+		return salary;
+	}
+
+
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+
+
+	public int getExp() {
+		return exp;
+	}
+
+	public void setExp(int exp) {
+		this.exp = exp;
+	}
+
+	public void workingOnProject()
+	{
+		// code
+		System.out.println
+		(empName+" working on "
+				+ ""+projectName);
+		
+	}
+	
+	
+	
+	
+
+}//end class
---
+++ Pattern3.java
@@ -0,0 +1,39 @@
+/*    *
+     *  *
+    *    *
+   ********  */
+package H1;
+import java.util.Scanner;
+public class Pattern1C {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("number Of Rows:");
+		int n=sc.nextInt();
+		int i, j, k;
+		for ( i=1;i<=n;i++)
+		{
+			for(j=i; j<n; j++)
+			{
+				System.out.print(" ");
+			}
+			
+			for( k=1; k<=(2*i-1); k++)
+			{
+				
+				if(k==1 || i==n || k==(2*i-1))
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print(" \n");
+		}
+		
+	}
+
+}
+
